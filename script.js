577647666f3e2e44241dd92db57ec60b07bbbb98
JanHuysmans <12201074@student.pxl.be>
2023-07-07 17:27:57 +0200

    13e Commit - Update - Buildr. page updates + aanpassingen navbar-top kleur en formaat.

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-    var toggle = document.querySelector('.navbar__toggle');
+    let toggle = document.querySelector('.navbar__toggle');
 
     toggle.addEventListener('click', function () {
         toggle.classList.toggle('open');
     });
 });
+
+/* When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar */
+let prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("navbar-top").style.top = "0";
+    } else {
+        document.getElementById("navbar-top").style.top = "-50px";
+    }
+    prevScrollpos = currentScrollPos;
+}
+
+
